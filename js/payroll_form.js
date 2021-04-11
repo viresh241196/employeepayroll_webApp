50f07bed488571c6114c1ce1a9bedf4b6f3ea1fb
viresh241196 <vireshrawool007@gmail.com>
2021-04-11 18:23:05 +0530

    added method to save the object into local storage

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -24,10 +24,27 @@ salary.addEventListener("input", function () {
 const save = () => {
   try {
     let employeePayrollData = createEmployeePayroll();
+    createAndUpdateStorage(employeePayrollData);
   } catch (e) {
     return;
   }
 };
+
+function createAndUpdateStorage(employeePayrollData) {
+  let employeePayrollList = JSON.parse(
+    localStorage.getItem("EmployeePayrollList")
+  );
+  if (employeePayrollList != undefined) {
+    employeePayrollList.push(employeePayrollData);
+  } else {
+    employeePayrollList = [employeePayrollData];
+  }
+  alert(employeePayrollList.toString());
+  localStorage.setItem(
+    "EmployeePayrollList",
+    JSON.stringify(employeePayrollList)
+  );
+}
 
 const createEmployeePayroll = () => {
   let employeePayrollData = new EmployeePayrollData();
