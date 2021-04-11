d9f966093262109e46ea3e4d782826b408be4d3b
viresh241196 <vireshrawool007@gmail.com>
2021-04-11 18:56:53 +0530

    added method to reset the form

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -88,3 +88,32 @@ const getInputElementValue = (id) => {
   let value = document.getElementById(id).value;
   return value;
 };
+
+const resetForm = () => {
+  setValue("#name", "");
+  unsetSelectedValues("[name=profile]");
+  unsetSelectedValues("[name=gender]");
+  unsetSelectedValues("[name=department]");
+  setValue("#salary", "");
+  setValue("#notes", "");
+  setValue("#day", "1");
+  setValue("#month", "January");
+  setValue("#year", "2020");
+};
+
+const unsetSelectedValues = (propertyValue) => {
+  let allItems = document.querySelectorAll(propertyValue);
+  allItems.forEach((item) => {
+    item.checked = false;
+  });
+};
+
+const setTextValue = (id, value) => {
+  const element = document.querySelector(id);
+  element.textContent = value;
+};
+
+const setValue = (id, value) => {
+  const elemnet = document.querySelector(id);
+  element.value = value;
+};
