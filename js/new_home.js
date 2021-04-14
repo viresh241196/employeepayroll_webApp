86c595685d0a71235f2cf0ada2927b0553dec088
viresh241196 <vireshrawool007@gmail.com>
2021-04-14 16:27:13 +0530

    added method and for loop to read the data form json

--- js/new_home.js
+++ js/new_home.js
@@ -1,32 +1,72 @@
 window.addEventListener("DOMContentLoaded", (event) => {
-    createInnerHtml();
-  });
+  createInnerHtml();
+});
 
-const createInnerHtml = () =>{
-    const headerHtml= "<th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th>";
-    const innerHtml = `${headerHtml}
+const createInnerHtml = () => {
+  const headerHtml =
+    "<th></th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th>";
+  let innerHtml = `${headerHtml}`;
+  let empPayrollList = createEmployeePayrollJSON();
+  for (const empPayrollData of empPayrollList) {
+     innerHtml = `${innerHtml}
     <tr>
       <td>
         <img
-          src="../assets/profile-images/Ellipse -3.png"
+          src="${empPayrollData._profilePic}"
           class="profile"
           alt=""
         />
       </td>
-      <td>Viresh Rawool</td>
-      <td>M</td>
+      <td>${empPayrollData._name}</td>
+      <td>${empPayrollData._gender}</td>
       <td>
-        <div class="dept-label">Sales</div>
-        <div class="dept-label">Finance</div>
-        <div class="dept-label">HR</div>
-      </td>
-      <td>3000000</td>
-      <td>1 Nov 2020</td>
+        ${getDeptHtml(empPayrollData._department)}</td>
+      <td>${empPayrollData._salary}</td>
+      <td>${empPayrollData._startDate}</td>
       <td>
-      <img src="../assets/icons/delete-black-18dp.svg" onclick="remove(this)" id="1" alt="delete">
-      <img src="../assets/icons/create-black-18dp.svg" onclick="update(this)" id="1" alt="edit">
-  </td>
+      <img src="../assets/icons/delete-black-18dp.svg" onclick="remove(this)" id="${
+        empPayrollData._id
+      }" alt="delete">
+      <img src="../assets/icons/create-black-18dp.svg" onclick="update(this)" id="${
+        empPayrollData._id
+      } " alt="edit">
+        </td>
     </tr>
     `;
-    document.querySelector("#table-display").innerHTML=innerHtml;
   }
+  document.querySelector("#table-display").innerHTML = innerHtml;
+};
+
+const createEmployeePayrollJSON = () => {
+  let empPayrollListLocal = [
+    {
+      _name: "Viresh Rawool",
+      _gender: "M",
+      _department: ["Engineering", "Finance"],
+      _salary: "500000",
+      _startDate: "29 Oct 2019",
+      _note: "",
+      _id: new Date().getTime(),
+      _profilePic: "assetsprofile-imagesEllipse -3.png",
+    },
+    {
+      _name: "Pooja Gangan",
+      _gender: "F",
+      _department: ["Engineering", "Finance"],
+      _salary: "900000",
+      _startDate: "29 Oct 2019",
+      _note: "",
+      _id: new Date().getTime() + 1,
+      _profilePic: "assetsprofile-imagesEllipse -1.png",
+    },
+  ];
+  return empPayrollListLocal;
+};
+
+const getDeptHtml = (deptList) => {
+  let deptHtml = "";
+  for (const dept of deptList) {
+    deptHtml = `${deptHtml}<div class="dept-label">${dept}</div>`;
+  }
+  return deptHtml;
+};
